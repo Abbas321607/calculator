fc51ba4b1497c0dbb739b3fc5f3fd4435df1003d
abbas321607 <zaheerabbassyed64@gmail.com>
2024-10-05 11:48:47 +0530

    first commit

---
+++ calculator/react.js/gni.js
@@ -0,0 +1,111 @@
+// console.log("welcome to javascript");
+// console.log("welcome to javascript");
+
+// let a=100,b=500;
+// if(a>b){
+//     console.log("a is greater than b");
+// }
+// else if(a<b){
+//     console.log("a is smaller than b");
+// }
+//conditional arthimetic logic
+
+// for(let i=0;i<=10;i++)
+//     {
+//         console.log("i value is="+i)
+//     }
+//     const a=10;
+// a++;
+// console.log(a+5);
+
+// let collegeName="gnit college";
+// console.log(collegeName);
+// console.log(collegeName.toLowerCase());
+// console.log(collegeName.toUpperCase());
+// console.log(collegeName.length);
+
+// let collegeName=new String("gnit college");
+// console.log(collegeName);
+// console.log(collegeName.toLowerCase());
+// console.log(collegeName.toUpperCase());
+// console.log(collegeName.length);
+
+// startWith():
+// let collegeName=new String("gnit college");
+// console.log(collegeName.startsWith("gnit"));
+// console.log(collegeName.startsWith("Gnit"));
+// console.log(collegeName.startsWith("college"));
+
+// endsWith():
+// let collegeName=new String("gnit college");
+// console.log(collegeName.endsWith("gnit"));
+// console.log(collegeName.endsWith("college"));
+// console.log(collegeName.endsWith("College"));
+
+// let collegeName=new String("gnit college");
+// console.log(collegeName.includes("gnit"));
+// console.log(collegeName.includes("college"));
+
+// let collegeName=new String("gnit college");
+// console.log(collegeName.repeat(2));
+
+// backtique:
+// let collegeName=new String("gnit college");
+// console.log(`hi now i am studying in ${collegeName} `);
+
+// class;
+// class Employee
+// {
+//     displayInfo()
+//     {
+//         console.log("hi this is display method")
+//     }
+// }
+// let emp=new Employee();
+// emp.displayInfo();
+// class Employee
+// {
+//     constructor()
+//     {
+//      this.id=101;
+//      this.ename="Ajay"
+//     }
+//     displayInfo()
+//     {
+//         console.log(this.id+" B "+this.ename);
+//     }
+// }
+// let emp=new Employee();
+// emp.displayInfo();
+// class Employee
+{
+//     constructor(id,ename)
+//     {
+//      this.id=id;
+//      this.ename=ename;
+//     }
+//     displayInfo()
+//     {
+//         console.log(this.id+" "+this.ename);
+//     }
+// }
+// let emp=new Employee(1001,"rajesh");
+// emp.displayInfo();
+// class Employee
+// {
+//     id=1001;
+//     ename="vinay kumar";
+//     displayInfo()
+//     {
+//         console.log(this.id+" "+this.ename);
+//     }
+// }
+// let emp=new Employee();
+// emp.displayInfo();
+
+// for each loop:
+// x=[10,20,30,40];
+// for(let i of x)
+// {
+//     console.log(i);
+// }
